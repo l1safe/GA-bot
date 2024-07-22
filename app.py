64dc6505f6604045c79b045552fbefd523eb2b2b
MaximKorolyov <max_klasiklmail.ru>
2024-07-23 01:39:21 +0300

    stage 1

--- app.py
+++ app.py
@@ -2,41 +2,72 @@ from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler, MessageHandler, filters, Updater
 from credentials import BOT_TOKEN, BOT_USERNAME
 import json
+from request import check_availability
 from bs4 import BeautifulSoup
 import requests
 
 
-async def launch_web_ui(update: Update, callback: CallbackContext):
-    await update.effective_chat.send_message("I hear you loud and clear !")
+def check(text: str) -> str:
+    if "https://goldapple.ru" in text:
+        return check_availability(text)
+    return "Полученна неизвестная команда. Для помощи напишите /help"
 
-async def help_command(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text('Help!')
+async def launch_web_ui(update: Update, callback: CallbackContext):         
+    await update.effective_chat.send_message("Привет, дружок-пирожок, я твой асистент! Напиши /help, чтобы узнать, что я умею") 
+                                                                            
+async def help_command(update: Update, context: CallbackContext) -> None:   
+    await update.message.reply_text("/start - Запустить бота \n /help - Список команд \n /go - Запросить ссылку"
+                                    )
 
-async def adding_in_array(update: Update, callback: CallbackContext):
-    message = update.message
-    text = message.text
+async def echo(update: Update, context: CallbackContext):
+    response: str = check(update.message.text)
+    await update.message.reply_text(response)
 
-    page = requests.get(text)
-    print(page.status_code)
-    soup = BeautifulSoup(page.text, "html.parser")
-    allNews = soup.findAll('div', class_='iBwy+ p0Qv4')
-    print(allNews)
-    for data in allNews:
-        text_content = data.text.strip() 
-        print(f"Found text: '{text_content}'")  
-        if text_content == 'нет в наличии':
-             await update.effective_chat.send_message(f'Нету в наличии')
-        else: 
-             await update.effective_chat.send_message(f'Можно заказать')
-   
-    print(f'{text}' )
-    await update.effective_chat.send_message(f'you sent {text}, {page.status_code}')
+async def go(update: Update, context: CallbackContext):
+    await update.message.reply_text('Введите ссылку')
+    
+
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', launch_web_ui))
     application.add_handler(CommandHandler('help', help_command))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, adding_in_array))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(CommandHandler('go', go))
+
+    
 
     print(f"Your bot is listening! Navigate to http://t.me/{BOT_USERNAME} to interact with it!")
     application.run_polling()
+
+
+
+
+
+
+# async def adding_in_array(update: Update, callback: CallbackContext):
+#     message = update.message
+#     text = message.text
+
+#     page = requests.get(text)
+#     print(page.status_code)
+#     soup = BeautifulSoup(page.text, "html.parser")
+#     allNews = soup.findAll('div', class_='iBwy+ p0Qv4')
+#     print(allNews)
+#     for data in allNews:
+#         text_content = data.text.strip() 
+#         print(f"Found text: '{text_content}'")  
+#         if text_content == 'нет в наличии':
+#              await update.effective_chat.send_message(f'Нету в наличии')
+#         else: 
+#              await update.effective_chat.send_message(f'Можно заказать')
+   
+#     print(f'{text}' )
+#     await update.effective_chat.send_message(f'you sent {text}, {page.status_code}')
+    
+
+
+
+
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, adding_in_array))
--- request.py
+++ request.py
@@ -1,16 +1,40 @@
+# from bs4 import BeautifulSoup
+# import requests
+
+# url = 'https://goldapple.ru/19000180989-lazy-soft-paw'
+
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
+# allNews = soup.find('button', class_='SjlhL _4oeL0 +a2KR Mh4g1 MtOhv')
+# print(allNews)
+# for data in allNews:
+#     text_content = data.text.strip() 
+#     print(f"Found text: '{text_content}'")  
+#     if text_content == 'нет в наличии':
+#         print('Нету в наличии')
+#     else: 
+#         print('Можно заказать')
+
+
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
-url = 'https://goldapple.ru/19000180989-lazy-soft-paw'
+url = "https://goldapple.ru/19000180989-lazy-soft-paw"
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-allNews = soup.find('button', class_='SjlhL _4oeL0 +a2KR Mh4g1 MtOhv')
-print(allNews)
-for data in allNews:
-    text_content = data.text.strip() 
-    print(f"Found text: '{text_content}'")  
-    if text_content == 'нет в наличии':
-        print('Нету в наличии')
-    else: 
-        print('Можно заказать')
+def check_availability(url):
+    driver = webdriver.Edge()
+    driver.get(url)
+    src = driver.page_source
+    driver.quit()
+    soup = BeautifulSoup(src, "lxml")
+    ne_v_nalichii = soup.find("button", class_="SjlhL _4oeL0 +a2KR Mh4g1 MtOhv")
+    if "узнать о поступлении" in ne_v_nalichii.text:
+        print("Нет в наличии")
+        return (url + " Нет в наличии")
+    elif "добавить в корзину":
+        print("Есть в наличии")
+        return (url + "Есть в наличии")
+    else:
+        print("Ошибка")
+        return "Ошибка"
+
